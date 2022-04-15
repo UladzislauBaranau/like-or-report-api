d3bf644ea2bee36c203675b7e725e9101b570847
Vladislav Baranov <uladzislaubaranau1996@gmail.com>
2022-04-15 13:30:38 +0300

    Start users app

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Profile(AbstractUser):
+    is_blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.username}, blocked: {self.is_blocked}"
+
+    class Meta:
+        ordering = ("id",)
