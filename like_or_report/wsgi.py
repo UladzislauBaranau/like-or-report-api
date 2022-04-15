e8f1476c500cf391785493203492195ab2dae4e3
Vladislav Baranov <uladzislaubaranau1996@gmail.com>
2022-04-15 13:41:33 +0300

    Add setup.cfg and apply code style settings

--- like_or_report/wsgi.py
+++ like_or_report/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'like_or_report.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "like_or_report.settings")
 
 application = get_wsgi_application()
