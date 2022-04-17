f2044b41d8ceb8542999143b0c1935a2a91edd5d
Uladzislau Baranau <uladzislaubaranau1996@gmail.com>
2022-04-17 14:41:57 +0300

    Customize admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import Profile
 
-admin.site.register(Profile)
+
+class CustomUserAdmin(UserAdmin):
+    model = Profile
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_blocked",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = (
+        "username",
+        "email",
+        "is_staff",
+        "is_blocked",
+    )
+
+
+admin.site.register(Profile, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 
 class Profile(AbstractUser):
-    is_blocked = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(
+        default=False,
+        help_text="Designates that this user has blocked.",
+    )
 
     def __str__(self):
         return f"{self.username}, blocked: {self.is_blocked}"
