dc79b923ee4e552ef4eb17509e8d73ba6aa26b5e
Vladislav Baranov <uladzislaubaranau1996@gmail.com>
2022-05-18 15:23:50 +0300

    Add tests for users

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from django.contrib.auth import get_user_model
 
+from rest_framework.test import APIClient
+
 User = get_user_model()
 
 
@@ -10,5 +12,13 @@ def test_user(db):
     user = User.objects.create_user(
         username="testuser",
         email="testemail@test.com",
+        password="testpass1",
     )
     return user
+
+
+@pytest.fixture
+def auth_client_user(test_user):
+    client = APIClient()
+    client.force_authenticate(test_user)
+    return client
--- tests/api/tests_users/tests_views.py
+++ tests/api/tests_users/tests_views.py
@@ -1,19 +1,21 @@
 from django.contrib.auth import get_user_model
 
+from rest_framework import status
 from rest_framework.test import APIClient
 
 User = get_user_model()
 client = APIClient()
 
 
-def test_success_user_register(db, test_user):
+def test_success_user_register(test_user):
     endpoint = "/api/v1/users/register/"
     request_data = {
         "username": "testuser1",
         "email": "testemail1@test.com",
         "password": "testpass1",
     }
-    response = client.post(endpoint, request_data)
+
+    client.post(endpoint, request_data)
 
     user = User.objects.get(username="testuser1")
     assert user.username == "testuser1"
@@ -21,3 +23,46 @@ def test_success_user_register(db, test_user):
 
     qs_users = User.objects.all()
     assert len(qs_users) == 2
+
+
+def test_fail_user_register(test_user):
+    endpoint = "/api/v1/users/register/"
+    request_data = {
+        "username": "testuser2",
+        "email": "testemail@test.com",  # this email belong test_user
+        "password": "testpass2",
+    }
+
+    response = client.post(endpoint, request_data)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    qs_users = User.objects.all()
+    assert len(qs_users) == 1
+
+
+def test_success_update_user_profile(auth_client_user, test_user):
+    pk = test_user.id
+    endpoint = f"/api/v1/users/update-profile/{pk}/"
+    request_data = {
+        "first_name": "unique_first_name",
+        "last_name": "unique_last_name",
+    }
+
+    response = auth_client_user.patch(endpoint, request_data)
+    assert response.status_code == status.HTTP_200_OK
+
+    user = User.objects.get(username="testuser")
+    assert user.first_name == "unique_first_name"
+    assert user.last_name == "unique_last_name"
+
+
+def test_change_user_password(auth_client_user, test_user):
+    pk = test_user.id
+    endpoint = f"/api/v1/users/password-change/{pk}/"
+    request_data = {
+        "old_password": "testpass1",
+        "new_password": "testpass11",
+    }
+
+    response = auth_client_user.patch(endpoint, request_data)
+    assert response.status_code == status.HTTP_200_OK
