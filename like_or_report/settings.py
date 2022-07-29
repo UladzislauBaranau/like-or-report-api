1bb7fec836ee57bafa29c9b6fd4708dec23b74f0
Uladzislau Baranau <uladzislaubaranau1996@gmail.com>
2022-07-29 21:58:20 +0300

    Add the variable 'DEBUG' to '.env.dev'

--- like_or_report/settings.py
+++ like_or_report/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env.dev"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(env("DEBUG"))
 
 ALLOWED_HOSTS = []
 
